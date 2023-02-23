bc23527737da6c1bb23089713e260ed9ec9c88dc
unknown <Piotrro36@gmail.com>
2023-02-23 12:02:29 +0100

    docs

--- app.py
+++ app.py
@@ -8,32 +8,46 @@ import os
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def hello() -> str:
+    """
+    Dummy endpoint to check if the app is online.
+
+    :return:
+    """
     return 'Hello from the PAAA home app!'
 
 
 @app.route("/run_pc", methods=['GET'])
 def run_pc_get():
+    """
+    Renders template with form to get data from user.
+
+    :return:
+    """
     return render_template('form.html')
 
 
 @app.route("/run_pc", methods=['POST'])
-def run_pc() -> str:
+def run_pc() -> Response:
+    """
+    Gets data from user, decrypts it and wakes up the PC.
+
+    :return:
+    """
     data = request.get_json()
     encrypted_mac = data.get("MAC")
-    key = os.environ.get('RUN_PC_PASSWD', "").encode()
+    key = os.environ.get('RUN_PC_PASSWD', "").encode()  # "" instead of None because encoding to bytes.
     if not key:
         print("No secret key in environ!!")
         return Response(status=405)
     mac_or_name = decrypt(encrypted_mac, key).decode()
-    mac = os.environ.get(mac_or_name.upper(), mac_or_name)
+    mac = os.environ.get(mac_or_name.upper(), mac_or_name)  # If shortcut was given by user.
     if mac:
-        # print(mac)
         wake_pc(mac)
     return Response(status=200)
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.0.136', port=5000, debug=False)
-    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='192.168.0.136', port=5000, debug=False)  # prod
+    # app.run(host='0.0.0.0', port=5000, debug=True)  # dev
--- wake_on_lan_tools.py
+++ wake_on_lan_tools.py
@@ -2,4 +2,12 @@ from wakeonlan import send_magic_packet
 
 
 def wake_pc(mac: str, port: int = 9) -> None:
+    """
+    Sends (broadcasts) "magic packet" in local network to run PC with given MAC address.
+    Remember: Your PC has to be configured to listen and wait for magic packets (wake on lan).
+
+    :param mac: MAC address of the PC you want to run.
+    :param port: On which port the packet should be sent (default is 9 in most PCs).
+    :return:
+    """
     send_magic_packet(mac, port=port)
